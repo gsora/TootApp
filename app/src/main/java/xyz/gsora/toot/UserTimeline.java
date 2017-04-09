35da9ad5eaf2064bc1b357a5e1564fb9314b3c22
Gianguido Sora <g.sora4@gmail.com>
2017-04-09 23:27:40 +0200

    actually do something! initialize the recyclerview, and load some mock data into it
    Signed-off-by: Gianguido Sora <g.sora4@gmail.com>

--- app/src/main/java/xyz/gsora/toot/UserTimeline.java
+++ app/src/main/java/xyz/gsora/toot/UserTimeline.java
@@ -2,12 +2,33 @@ package xyz.gsora.toot;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class UserTimeline extends AppCompatActivity {
+
+    private static final String TAG = UserTimeline.class.getSimpleName();
+
+    RecyclerView statusList;
+    StatusesListAdapter adpt;
+    LinearLayoutManager llm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_timeline);
+        setTitle("Timeline");
+
+        Mocker m = new Mocker(this);
+        statusList = (RecyclerView) findViewById(R.id.statuses_list);
+        llm = new LinearLayoutManager(getApplicationContext());
+        DividerItemDecoration dividerItemDecoration = new DividerItemDecoration(statusList.getContext(),
+                llm.getOrientation());
+        statusList.setLayoutManager(llm);
+        statusList.addItemDecoration(dividerItemDecoration);
+        adpt = new StatusesListAdapter(m.getMockStatuses(), this.getApplicationContext());
+        statusList.setAdapter(adpt);
+
     }
 }
