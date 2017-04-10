8710600ef5eec51d3b1368731b61395b7aabbaea
Gianguido Sora <g.sora4@gmail.com>
2017-04-10 13:39:01 +0200

    an hacky way to remove newlines at the end of Html.fromHtml() output; plus, no more ugly ifs to check android api level on the controller
    Signed-off-by: Gianguido Sora <g.sora4@gmail.com>

---
+++ app/src/main/java/xyz/gsora/toot/CoolHtml.java
@@ -0,0 +1,29 @@
+package xyz.gsora.toot;
+
+import android.text.Html;
+import android.text.SpannableString;
+import android.text.Spanned;
+
+/**
+ * Created by gsora on 4/10/17.
+ */
+public class CoolHtml {
+
+    public static Spanned html(String s, int i) {
+        Spanned old;
+
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.N) {
+            old = Html.fromHtml(s, i);
+        } else {
+            old = Html.fromHtml(s);
+        }
+
+        CharSequence text = old;
+        while (text.charAt(text.length() - 1) == '\n') {
+            text = text.subSequence(0, text.length() - 1);
+        }
+
+        Spanned sp = new SpannableString(text);
+        return sp;
+    }
+}
