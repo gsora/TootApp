a292fd0e65af7f3f31de8a0349b5c73787724233
Gianguido Sorà <g.sora4@gmail.com>
2017-04-13 19:18:51 +0200

    pass the current system locale to the list adapter, plus correct time formatting! ...and info on how to run API calls via postman

--- app/src/main/java/xyz/gsora/toot/StatusesListAdapter.java
+++ app/src/main/java/xyz/gsora/toot/StatusesListAdapter.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,8 +16,10 @@ import butterknife.ButterKnife;
 import com.squareup.picasso.Picasso;
 import de.hdodenhof.circleimageview.CircleImageView;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.*;
 
 /**
  * Created by gsora on 4/9/17.
@@ -29,9 +32,11 @@ public class StatusesListAdapter extends RecyclerView.Adapter<StatusesListAdapte
 
     private ArrayList<Status> statuses;
     private Context parentCtx;
+    private static String systemLocale;
 
-    public StatusesListAdapter(Status[] s, Context parentCtx) {
+    public StatusesListAdapter(Status[] s, Context parentCtx, String locale) {
         statuses = new ArrayList<Status>(Arrays.asList(s));
+        systemLocale = locale;
         this.parentCtx = parentCtx;
     }
 
@@ -84,14 +89,32 @@ public class StatusesListAdapter extends RecyclerView.Adapter<StatusesListAdapte
             holder.boostAuthor.setTextSize(12.0f);
         }
 
-        /*SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-ddTHH:MM:SS");
+        // format the timestamp according to the device's setting
+        SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.forLanguageTag(systemLocale));
+        Date d = null;
         try {
-            Log.d(TAG, fmt.parse(timestamp).toString());
+            d = fmt.parse(timestamp);
         } catch (ParseException e) {
             e.printStackTrace();
-        }*/
+        }
 
-        holder.timestamp.setText(timestamp);
+        DateFormat dateFormat = android.text.format.DateFormat.getLongDateFormat(parentCtx);
+        DateFormat timeFormat = android.text.format.DateFormat.getTimeFormat(parentCtx);
+
+        String finalTimestamp = null;
+        Calendar cal1 = Calendar.getInstance();
+        Calendar cal2 = Calendar.getInstance();
+        cal1.setTime(d);
+        cal2.setTime(new Date());
+        boolean sameDay = cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) && cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR);
+
+        if(!sameDay) {
+            finalTimestamp = timeFormat.format(d) + " - " + dateFormat.format(d);
+        } else {
+            finalTimestamp = timeFormat.format(d);
+        }
+
+        holder.timestamp.setText(finalTimestamp);
     }
 
     // Provide a reference to the views for each data item
