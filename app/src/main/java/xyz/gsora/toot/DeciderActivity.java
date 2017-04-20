b584bc4947c3e4d67c65e1a26c92c748b05a2e68
Gianguido Sora <g.sora4@gmail.com>
2017-04-20 16:56:10 +0200

    added an activity to check if the user is logged in or not
    Signed-off-by: Gianguido Sora <g.sora4@gmail.com>

---
+++ app/src/main/java/xyz/gsora/toot/DeciderActivity.java
@@ -0,0 +1,20 @@
+package xyz.gsora.toot;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class DeciderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_decider);
+
+        if (Toot.hasLoggedIn()) {
+            startActivity(new Intent(this, UserTimeline.class));
+        } else {
+            startActivity(new Intent(this, LoginActivity.class));
+        }
+    }
+}
