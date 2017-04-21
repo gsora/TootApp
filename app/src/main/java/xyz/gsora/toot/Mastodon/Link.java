b3fe57d4c250b5dfa9f81f9d5a7c6d94e31f83b9
Gianguido Sora <g.sora4@gmail.com>
2017-04-22 00:37:54 +0200

    added some Link header parsing facilities fuck you java, really
    Signed-off-by: Gianguido Sora <g.sora4@gmail.com>

---
+++ app/src/main/java/xyz/gsora/toot/Mastodon/Link.java
@@ -0,0 +1,39 @@
+package xyz.gsora.toot.Mastodon;
+
+public class Link {
+
+    private String URL;
+    private LinkParser.Relationship rel;
+
+    public Link(String URL, LinkParser.Relationship rel) {
+        this.URL = URL;
+        this.rel = rel;
+    }
+
+    public Link() {
+    }
+
+    public LinkParser.Relationship getRel() {
+        return rel;
+    }
+
+    public void setRel(LinkParser.Relationship rel) {
+        this.rel = rel;
+    }
+
+    public String getURL() {
+        return URL;
+    }
+
+    public void setURL(String URL) {
+        this.URL = URL;
+    }
+
+    @Override
+    public String toString() {
+        return "Link{" +
+                "URL='" + URL + '\'' +
+                ", rel=" + rel +
+                '}';
+    }
+}
---
+++ app/src/main/java/xyz/gsora/toot/Mastodon/LinkParser.java
@@ -0,0 +1,52 @@
+package xyz.gsora.toot.Mastodon;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class LinkParser {
+
+    private static final Pattern URLPattern = Pattern.compile("<(.*?)>");
+    private static final Pattern RelPattern = Pattern.compile("rel=\"(\\w+)\"");
+
+    public static Link parseNext(String links) {
+        String[] commas = splitStringByComma(links);
+        return parseLinkRelationship(splitStringBySemicolon(commas[0]));
+    }
+
+    public static Link parsePrev(String links) {
+        String[] commas = splitStringByComma(links);
+        return parseLinkRelationship(splitStringBySemicolon(commas[1]));
+    }
+
+    private static String[] splitStringByComma(String links) {
+        return links.split(",");
+    }
+
+    private static String[] splitStringBySemicolon(String link) {
+        return link.split(";");
+    }
+
+    private static Link parseLinkRelationship(String[] linkRel) {
+        Link l = new Link();
+        Matcher URLMatcher = URLPattern.matcher(linkRel[0]);
+        if (URLMatcher.find()) {
+            l.setURL(URLMatcher.group(1));
+            Matcher RelMatcher = RelPattern.matcher(linkRel[1]);
+            RelMatcher.find();
+            switch (RelMatcher.group(1)) {
+                case "next":
+                    l.setRel(Relationship.NEXT);
+                    break;
+                case "prev":
+                    l.setRel(Relationship.PREV);
+                    break;
+            }
+        }
+        return l;
+    }
+
+    public enum Relationship {
+        NEXT,
+        PREV
+    }
+}
