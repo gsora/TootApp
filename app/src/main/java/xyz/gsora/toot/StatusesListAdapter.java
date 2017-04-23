bd35c1c40d7023b8ebdecf437dac39fd3b54d842
Gianguido Sora <g.sora4@gmail.com>
2017-04-23 19:34:22 +0200

    handle boosts as they should be.
    Signed-off-by: Gianguido Sora <g.sora4@gmail.com>

--- app/src/main/java/xyz/gsora/toot/StatusesListAdapter.java
+++ app/src/main/java/xyz/gsora/toot/StatusesListAdapter.java
@@ -29,36 +29,39 @@ import java.util.Locale;
  * <p>
  * Custom adapter, useful for listing some statuses.
  */
+@SuppressWarnings("ConstantConditions")
 public class StatusesListAdapter extends RealmRecyclerViewAdapter<Status, StatusesListAdapter.ViewHolder> {
 
     private static final String TAG = StatusesListAdapter.class.getSimpleName();
     private static String systemLocale;
-    private Context parentCtx;
+    private final Context parentCtx;
 
     public StatusesListAdapter(RealmResults<Status> data, String locale, Context parentCtx) {
         super(data, true);
         systemLocale = locale;
         this.parentCtx = parentCtx;
+        setHasStableIds(true);
     }
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.status_row_layout, parent, false);
-        ViewHolder vh = new ViewHolder(v);
-        return vh;
+        return new ViewHolder(v);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Status s = getItem(position);
+        holder.data = getItem(holder.getAdapterPosition());
+        Status s = holder.data;
         Boost sb = s.getReblog();
 
-        if (sb != null) { // this is a boost
+        if (s.getThisIsABoost()) { // this is a boost
             setStatusViewTo(sb.getAccount().getDisplayName(), sb.getContent(), sb.getAccount().getAvatar(), s.getAccount().getDisplayName(), sb.getCreatedAt(), holder);
         } else {
             setStatusViewTo(s.getAccount().getDisplayName(), s.getContent(), s.getAccount().getAvatar(), null, s.getCreatedAt(), holder);
         }
+
     }
 
     private void setStatusViewTo(String author, String content, String avatar, String booster, String timestamp, StatusesListAdapter.ViewHolder holder) {
@@ -66,18 +69,7 @@ public class StatusesListAdapter extends RealmRecyclerViewAdapter<Status, Status
         holder.statusAuthor.setText(CoolHtml.html(author));
         holder.status.setText(CoolHtml.html(content));
 
-
         Picasso.with(parentCtx).load(avatar).into(holder.avatar);
-
-        if (booster != null) {
-            LinearLayout.LayoutParams p = (LinearLayout.LayoutParams) holder.boostAuthor.getLayoutParams();
-            p.setMargins(0, 0, 0, 16);
-            holder.boostAuthor.setLayoutParams(p);
-
-            holder.boostAuthor.setVisibility(View.VISIBLE);
-            holder.boostAuthor.setText("Boosted by " + booster);
-            holder.boostAuthor.setTextSize(12.0f);
-        }
 
         // format the timestamp according to the device's setting
         SimpleDateFormat fmt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.forLanguageTag(systemLocale));
@@ -91,26 +83,47 @@ public class StatusesListAdapter extends RealmRecyclerViewAdapter<Status, Status
         DateFormat dateFormat = android.text.format.DateFormat.getLongDateFormat(parentCtx);
         DateFormat timeFormat = android.text.format.DateFormat.getTimeFormat(parentCtx);
 
-        String finalTimestamp = null;
+        String finalTimestamp;
         Calendar cal1 = Calendar.getInstance();
         Calendar cal2 = Calendar.getInstance();
         cal1.setTime(d);
         cal2.setTime(new Date());
         boolean sameDay = cal1.get(Calendar.YEAR) == cal2.get(Calendar.YEAR) && cal1.get(Calendar.DAY_OF_YEAR) == cal2.get(Calendar.DAY_OF_YEAR);
 
-        if(!sameDay) {
+        if (!sameDay) {
             finalTimestamp = timeFormat.format(d) + " - " + dateFormat.format(d);
         } else {
             finalTimestamp = timeFormat.format(d);
         }
 
         holder.timestamp.setText(finalTimestamp);
+
+        if (booster != null) {
+            if (BuildConfig.DEBUG) {
+                holder.timestamp.setText(holder.timestamp.getText().toString() + " - boosted by " + booster);
+            }
+            LinearLayout.LayoutParams p = (LinearLayout.LayoutParams) holder.boostAuthor.getLayoutParams();
+            p.setMargins(0, 0, 0, 16);
+            holder.boostAuthor.setLayoutParams(p);
+            holder.boostAuthor.setVisibility(View.VISIBLE);
+            holder.boostAuthor.setText("Boosted by " + booster);
+            holder.boostAuthor.setTextSize(12.0f);
+        } else {
+            holder.boostAuthor.setText("");
+        }
+    }
+
+    @SuppressWarnings("ConstantConditions")
+    @Override
+    public long getItemId(int index) {
+        return getItem(index).getId();
     }
 
     // Provide a reference to the views for each data item
     // Complex data items may need more than one view per item, and
     // you provide access to all the views for a data item in a view holder
     public static class ViewHolder extends RecyclerView.ViewHolder {
+        public Status data;
         // each data item is just a string in this case
         @BindView(R.id.status_author)
         TextView statusAuthor;
@@ -125,13 +138,11 @@ public class StatusesListAdapter extends RealmRecyclerViewAdapter<Status, Status
 
         public ViewHolder(View v) {
             super(v);
+            data = null;
             ButterKnife.bind(this, v);
             status.setMovementMethod(LinkMovementMethod.getInstance());
         }
-    }
 
-    /*
-    Private methods
-     */
+    }
 
 }
